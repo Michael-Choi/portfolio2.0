bf3c631eb05f3a18ff3a3ba1bef9ca671a3c2966
Michael Choi <michael.choi@queensu.ca>
2025-06-28 14:05:57 -0400

    update readme

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Header } from '@/components/Header';
 import { Hero } from '@/components/Hero';
 import { About } from '@/components/About';
 import { Experience } from '@/components/Experience';
-import { Projects } from '@/components/Projects';
+// import { Projects } from '@/components/Projects';
 import { Contact } from '@/components/Contact';
 import { Footer } from '@/components/Footer';
 
